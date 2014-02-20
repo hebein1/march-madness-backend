e1d3b467e701a99fb7f53d80207e99464e911804
hebein1 <hebein1@illinois.edu>
2014-02-20 09:47:54 -0600

    added unit tests for python library

--- python/py_test.py
+++ python/py_test.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
 
-##   construct_dt_and_classify_one_sample_case2.py
-
-##  This script is for a mixture of symbolic and numeric
-##  features.  Since we have numeric features, only a csv
-##  file can be used for training.  Note how we tell the
-##  module that the class label in each training record is
-##  placed in the column indexed 2 and how the feature are
-##  to be found in columns indexed 3, 4, 5, 6, 7, and 8.
-
-##  Remember, the column indexing in the csv file is
-##  zero-based.  That is, the first column is indexed 0.
-
-##  The training file `stage3cancer.csv' was taken from the
-##  RPART module by Terry Therneau and Elizabeth Atkinson.
-##  This module is a part of the R based statistical package
-##  for classification and regression by recursive
-##  partitioning of data.
-
 import DecisionTree
-import sys
 
 training_datafile = "py_test_data.csv"
 
@@ -34,14 +15,10 @@ dt.get_training_data()
 dt.calculate_first_order_probabilities()
 dt.calculate_class_priors()
 
-#   UNCOMMENT THE FOLLOWING LINE if you would like to see the training
-#   data that was read from the disk file:
 #dt.show_training_data()
 
 root_node = dt.construct_decision_tree_classifier()
 
-#   UNCOMMENT THE FOLLOWING LINE if you would like to see the decision
-#   tree displayed in your terminal window:
 print("\n\nThe Decision Tree:\n")
 root_node.display_decision_tree("     ")
 
@@ -51,8 +28,6 @@ test_sample  = ['g2 = 4.2',
                 'eet = 1.7',
                 'age = 55.0',
                 'ploidy = diploid']
-
-# The result of the script is for displaying the results:
 
 classification = dt.classify(root_node, test_sample)
 
@@ -69,5 +44,3 @@ for which_class in which_classes:
 
 print("\nSolution path in the decision tree: " + str(solution_path))
 print("\nNumber of nodes created: " + str(root_node.how_many_nodes()))
-
-
