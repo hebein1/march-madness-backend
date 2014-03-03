69708e8a116688ce3a30ce978f831625cc5eb564
Siddharth Shukramani <siddshuk.illinois@gmail.com>
2014-03-03 04:18:09 -0600

    Updated training and test. Removed fixed point tests. Data file now generated using ruby script

---
+++ neuralNetwork/NCAA/randomData.rb
@@ -0,0 +1,25 @@
+File.open('ncaa.data', 'w') do |file|
+	i = 0
+	str = "2000 2 1\n"
+	while i<2000 do
+		pair = ""
+		rand1 = Random.rand(50...80)
+		pair += rand1.to_s + " "
+		rand2 = Random.rand(50...80)
+		pair += rand2.to_s + "\n"
+		
+		if rand1 >= rand2
+			pair += 1.to_s + "\n"
+		else
+			pair += 0.to_s + "\n"
+		end
+		
+		str += pair
+
+		i += 1
+
+	end
+
+	file.write(str)
+end
+ 
