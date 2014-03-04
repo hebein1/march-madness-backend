1ca2585b628a73de21c9f7c4b5e26681dfa59772
Siddharth Shukramani <siddshuk.illinois@gmail.com>
2014-03-04 16:35:57 -0600

    Forgot to add ncaa_test.cpp file

---
+++ neuralNetwork/NCAA_ANN_src/ncaa_test.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include "fann.h"
+
+int main()
+{
+	fann_type *calc_out;
+	unsigned int i;
+	int ret = 0;
+	int num_test_passed = 0;
+	int num_total_tests = 0;
+	struct fann *ann;
+	struct fann_train_data *data;
+
+	printf("Creating network.\n");
+
+	ann = fann_create_from_file("ncaa_float.net");
+
+	if(!ann)
+	{
+		printf("Error creating ann --- ABORTING.\n");
+		return -1;
+	}
+
+	fann_print_connections(ann);
+	fann_print_parameters(ann);
+
+	printf("Testing network.\n");
+
+	data = fann_read_train_from_file("ncaa.data");
+
+	for(i = 0; i < fann_length_train_data(data); i++)
+	{
+		fann_reset_MSE(ann);
+		calc_out = fann_test(ann, data->input[i], data->output[i]);
+
+		printf("Matchup scores test (%f, %f) -> %f, should be %f, difference=%f\n",
+			   data->input[i][0], data->input[i][1], calc_out[0], data->output[i][0],
+			   (float) fann_abs(calc_out[0] - data->output[i][0]));
+		num_total_tests++;
+		if(((float) fann_abs(calc_out[0] - data->output[i][0])) < 0.5)
+			num_test_passed++;
+	}
+
+	float accuracy = ((float)num_test_passed) / ((float) num_total_tests);
+	printf("Accuracy = %f percent\n", accuracy*100.00);
+
+	printf("Cleaning up.\n");
+	fann_destroy_train(data);
+	fann_destroy(ann);
+
+	return ret;
+}
