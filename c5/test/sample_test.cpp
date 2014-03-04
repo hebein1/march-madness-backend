8b6efbef21e9c951f360703ccaf3328d76b2783d
hebein1 <hebein1@illinois.edu>
2014-03-03 20:51:11 -0600

    small edit to sample_test.cpp

--- c5/test/sample_test.cpp
+++ c5/test/sample_test.cpp
@@ -13,24 +13,24 @@ TEST(MMP, testSampleApplication)
 
 	/* generate the decision tree if it has not been made already */
 	if( access( "march_madness.tree", F_OK ) == -1 ) {
-   		FILE* pipe = popen("../c5.0 -f march_madness", "r");
-    	if(pipe)
+		FILE* pipe = popen("../c5.0 -f march_madness", "r");
+		if(pipe)
 		{
-    		pclose(pipe);
+			pclose(pipe);
 		}
 	}
 	
 	/* run the decision tree on the march_madness.cases data */
 	char buffer[128];
-    std::string result = "";
+	std::string result = "";
 	pipe = popen("./sample -f march_madness", "r");
-    if(pipe)
+	if(pipe)
 	{
-    	while(!feof(pipe)) {
-    		if(fgets(buffer, 128, pipe) != NULL)
-    			result += buffer;
-    	}
-    	pclose(pipe);
+		while(!feof(pipe)) {
+			if(fgets(buffer, 128, pipe) != NULL)
+				result += buffer;
+		}
+		pclose(pipe);
 	}
 
 	/* check output */
@@ -39,7 +39,8 @@ TEST(MMP, testSampleApplication)
 	std::getline(f, line); // skip line 1
 	std::getline(f, line); // skip line 2
 	std::getline(f, line); // skip line 3
-    while (std::getline(f, line)) {
+	while (std::getline(f, line)) {
+		std::cout << line << "\n";
 		ASSERT_TRUE(line.find("A") != std::string::npos || line.find("B") != std::string::npos);
-    }
+	}
 }
