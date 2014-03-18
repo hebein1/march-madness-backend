50654c769cca6dfd456bea3485f7df6b980d4665
Siddharth Shukramani <siddshuk.illinois@gmail.com>
2014-03-18 13:58:03 -0500

    Added tests

--- neuralNetwork/NCAA_ANN_src/annTest.cpp
+++ neuralNetwork/NCAA_ANN_src/annTest.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
-#include "ncaa_train.h"
 #include "gtest/gtest.h"
+#include "ncaa_train.h"
+#include "ncaa_test.h"
 
 TEST(ANN, checkNetworkCreation)
 {
 	ASSERT_TRUE(createNetwork(3, 2, 4, 1));
+}	
+
+TEST(PARSE, checkTeamStatSize)
+{
+	parse_teams("teams.txt");
+	
+	//check if size is right	
+	ASSERT_EQ((int)team_stats.size(), 351);	
 }
+
+TEST(PARSE, checkStatRankSizeEQ)
+{
+	parse_teams("teams.txt");
+
+	//check if sizes are equal
+	ASSERT_EQ(team_rank.size(), team_stats.size());	
+}
+
+TEST(PARSE, checkStatValNotEmpty)
+{
+	parse_teams("teams.txt");
+
+	//check if all keys have a value which is not empty
+	map<string, vector<float> >::const_iterator stat_it;
+	for(stat_it = team_stats.begin(); stat_it != team_stats.end(); ++stat_it)
+	{	
+		ASSERT_TRUE(!(stat_it->second).empty());
+	}
+}
+
+TEST(PARSE, checkRankValZero)
+{
+	parse_teams("teams.txt");
+
+	//check if all keys have a value equal to 0
+	map<string, int>::const_iterator rank_it;
+	for(rank_it = team_rank.begin(); rank_it != team_rank.end(); ++rank_it)
+	{	
+		ASSERT_EQ(rank_it->second, 0);
+	}
+}
+
--- neuralNetwork/NCAA_ANN_src/ncaa_test.cpp
+++ neuralNetwork/NCAA_ANN_src/ncaa_test.cpp
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <unistd.h>
-#include <map>
-#include <vector>
-#include <string>
-#include <sstream>
-#include "fann.h"
-
-using namespace std;
-
-map<string, int> team_rank;
-map<string, vector<float> > team_stats;
-	
+#include "ncaa_test.h"
 
 void print_rank()
 {
@@ -49,48 +35,6 @@ void print_rank()
 			printf("%d. %s => %d\n", i, (*it_vec).c_str(), (-1)*result_it->first);
 		}
 	}
-}
-
-void parse_teams(char * fileName)
-{
-	FILE *teams_file = fopen(fileName, "r");
-        if (teams_file == NULL)
-                perror("Teams file is empty");
-
-        char * line = NULL;
-        size_t len = 0;
-        size_t read;
-	char * pch;
-       	int pch_flag = 0;
-	string team_name;
-	while ((read = getline(&line, &len, teams_file)) != -1)
-        {
-	
-		if(pch_flag == 0)
-		{
-			team_name  = string(line, strlen(line)-1);
-			pch_flag = 1;
-		}
-		else
-		{
-			vector<float> avg_stats;
-			pch = strtok(line, " ");
-			while(pch != NULL)
-                	{
-				float stat = 0.0;
-                                istringstream (pch) >> stat;
-				avg_stats.push_back(stat);	
-                        
-                        	pch = strtok(NULL, " ");
-			}
-			pch_flag = 0;	
-               		team_rank[team_name] = 0;
-			team_stats[team_name] = avg_stats;
-		}
-		
-	}
-	
-	//print_rank();
 }
 
 int main()
